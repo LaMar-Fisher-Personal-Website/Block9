f26d7582bf78472f2b07dc5f01e3d15b1c88551b
LaMar Fisher <lsfisher503@gmail.com>
2023-03-30 23:51:31 -0700

    deleted a line on js

--- myscript.js
+++ myscript.js
@@ -2,7 +2,6 @@ function myFunction() {
     document.getElementById("demo").innerHTML = "Added Javascript"
     } 
     
-
       const myButton = document.getElementById("myButton");
       myButton.addEventListener("click", function() {
         alert("I am learning to use AI to assist me in understanding js");
